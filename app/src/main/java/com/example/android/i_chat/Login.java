e435c10e332d19b0d596a56b580b619ad36c4f0e
shivanshsoni <shivanshsoni23@gmail.com>
2018-03-19 19:36:25 +0530

    qbjef

--- app/src/main/java/com/example/android/i_chat/Login.java
+++ app/src/main/java/com/example/android/i_chat/Login.java
@@ -1,5 +1,6 @@
 package com.example.android.i_chat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by shivansh on 18/3/18.
@@ -44,7 +58,51 @@ public class Login extends AppCompatActivity {
                 if(name.isEmpty() || name.length() < 5) {
                     username.setError("username doesn't exist");
                 }
-                else{
+                else if(pass.length() < 6){
+                    password.setError("Invalid pasword");
+                }
+                else {
+                    String url = "https://ichat-81fb0.firebaseio.com/users.json";
+                    final ProgressDialog pd = new ProgressDialog(Login.this);
+                    pd.setMessage("Loading...");
+                    pd.show();
+
+                    StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String s) {
+                            if (s.equals("null")) {
+                                Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                            } else {
+                                try {
+                                    JSONObject obj = new JSONObject(s);
+
+                                    if (!obj.has(name)) {
+                                        Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                                    } else if (obj.getJSONObject(name).getString("password").equals(pass)) {
+                                        UserDetails.username = name;
+                                        UserDetails.password = pass;
+                                        startActivity(new Intent(Login.this, Users.class));
+                                    } else {
+                                        Toast.makeText(Login.this, "incorrect password", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            pd.dismiss();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            System.out.println("" + volleyError);
+                            pd.dismiss();
+                        }
+                    });
+
+                    RequestQueue rQueue = Volley.newRequestQueue(Login.this);
+                    rQueue.add(request);
+
 
                 }
             }
--- app/src/main/java/com/example/android/i_chat/Users.java
+++ app/src/main/java/com/example/android/i_chat/Users.java
@@ -1,8 +1,11 @@
 package com.example.android.i_chat;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by shivansh on 18/3/18.
  */
 
-public class Users {
+public class Users extends AppCompatActivity{
+
 }
